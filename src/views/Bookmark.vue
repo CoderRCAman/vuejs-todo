6e1b1b34a13507f905f8bfcde451c35f6e5e7681
coderRCAman <www.aman.k.o.12345@gmail.com>
2024-05-10 15:43:52 +0530

    some-fix

--- src/views/Bookmark.vue
+++ src/views/Bookmark.vue
@@ -2,7 +2,7 @@
     <h1 v-if="Bookmarks.length==0" class="text-lg text-center mt-10 text-sky-500">
         No Bookmarks to show!
     </h1>
-    <div v-else class="px-2" v-for="(bookmark, index) in Bookmarks" :key="index">
+    <div v-else class="px-2 container mx-auto" v-for="(bookmark, index) in Bookmarks" :key="index">
 
         <BookMarkItem :title="bookmark.title" :id="bookmark.id" :completed="bookmark.completed" />
     </div>
